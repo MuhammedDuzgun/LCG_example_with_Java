fcdb7c7d797aa5bf07c9c588ba91afdb9d505e69
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-11-22 20:33:53 +0300

    modify code

--- src/main/java/com/muhammed/Main.java
+++ src/main/java/com/muhammed/Main.java
@@ -8,20 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double x1 = lcgUret(x0, a, c, mod);
-        double x2 = lcgUret(x1, a, c, mod);
+        double x1 = lcg(x0, a, c, mod);
+        double x2 = lcg(x1, a, c, mod);
+
+        //before normalization
+        System.out.println("before normalization process X1 : " + x1);
+        System.out.println("before normalization process X2 : " + x2);
 
         //normalization
         x1 = x1 / mod;
         x2 = x2 / mod;
 
-        System.out.println("X1 : " + x1);
-        System.out.println("X2 : " + x2);
+        System.out.println("after normalization process X1 : " + x1);
+        System.out.println("after normalization process X2 : " + x2);
 
     }
 
-    public static double lcgUret(double x0, double a, int c, double mod) {
-        return (a * x0 + c) % mod;
+    public static double lcg(double xBaslangic, double a, int c, double mod) {
+        return (a * xBaslangic + c) % mod;
     }
 
 }
